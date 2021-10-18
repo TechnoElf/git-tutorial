ae7f67acb223d37903822678d275e944820f526d
TechnoElf <technoelf@undertheprinter.com>
2021-10-18 19:30:11 +0200

    Implemented challenge 2

--- src/main.c
+++ src/main.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
+#include "espl_lib.h"
+
 int main() {
-    printf("Hello ESPL\n");
+    char cont = 'y';
+    while (cont == 'y') {
+        int in = 0;
+        printf("Enter a number\n");
+        if (scanf(" %d", &in) != 1) { return 1; }
+
+        char* text = num_to_words(in);
+        printf("%s\n", text);
+
+	printf("Do you wish to enter another number? (y/n)\n");
+        if (scanf(" %c", &cont) != 1) { return 1; }
+    }
+
     return 0;
 }
